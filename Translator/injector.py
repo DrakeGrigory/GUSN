c4019b4d933af429227d075839de402f2cfbdae1
DrakeGrigory <grzegorz.kaczorowski2126@gmail.com>
2025-06-12 18:52:45 +0200

    injector done as a function

--- Translator/injector.py
+++ Translator/injector.py
@@ -1,40 +1,17 @@
 import numpy as np
+#INJECTOR - adds an error in a single bit of array.
+#Since my case is symetrical (X and O) it only injects in the 1/4 of the martix including middle row and collumn
+
+#Test matrix
+#arr1 = np.array([[0,1,0],
+#                 [1,0,1],
+#                 [0,1,0]])
 
 
-#arr1 = np.array([[0,1,2],
-#                  [3,4,5],
-#                  [6,7,8]])
+def injector(arg_org,printResult):
+    arr_len_half = (len(arg_org)>>1)+1
+    units = arr_len_half * arr_len_half
 
-arr1 = np.array([[0,1,0],
-                 [1,0,1],
-                 [0,1,0]])
-
-#
-arr_len_half = (len(arr1)>>1)+1
-units = arr_len_half * arr_len_half
-
-arr3D = np.zeros([units,len(arr1),len(arr1)],dtype=bool)
-for u in range(0, units):
-    arr3D[u]=arr1
-    print(arr3D[u])
-
-print("len_cut: " + str(arr_len_half))
-print(arr1)
-
-
-cnt=0
-for j_rows in range(0, arr_len_half):
-    for i_colls in range(0, arr_len_half):
-        arr3D[(cnt),j_rows,i_colls] = ~(arr3D[(cnt),j_rows,i_colls])
-        cnt = cnt + 1
-
-
-print("It executed "+str(cnt)+" times")
-print("\nOutput of arr1:\n"); print(arr3D)        
-
-
-
-def injector(arg_org):
     arr3D = np.zeros([units,len(arg_org),len(arg_org)],dtype=bool)
     for u in range(0, units):
         arr3D[u]=arg_org
@@ -45,6 +22,10 @@ def injector(arg_org):
             arr3D[(cnt),j_rows,i_colls] = ~(arr3D[(cnt),j_rows,i_colls])
             cnt = cnt + 1
 
+    if(printResult):
+        print("\n Input matrix:\n" + str(arg_org) +"\n\nOutput of injector:\n" + str(arr3D))
+    
     return arr3D
 
-arr3D_2 = injector(arr1)
+#Test call
+#arr3D_2 = injector(arr1,1)
